a4f962a5348336d75b33648d44b8f4ef38e7c538
adamakhm <akhadam00@gmail.com>
2025-04-22 18:12:44 +0300

    Created two files (base_page.py and main_page, in base_page.py created new class

---
+++ base_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
